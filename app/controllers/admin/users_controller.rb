84afba1010c85ccedcfe7d33c3b2e40ac88ebb4c
willow-swamp <yusuke.y19970103@outlook.com>
2023-05-14 00:40:01 +0900

    add admin_action

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,52 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+      # _urlで絶対パスで指定している。_pathだと相対パスになる。
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+      # _urlで絶対パスで指定している。_pathだと相対パスになる。
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+    # _urlで絶対パスで指定している。_pathだと相対パスになる。
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+end
